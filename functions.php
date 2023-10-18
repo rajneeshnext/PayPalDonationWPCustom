b644d5ad33dc2545ac13885c6c895bdfa60f2a16
Rajneesh Kumar Saini <rajnish123saini@gmail.com>
2023-10-18 15:18:20 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -18,37 +18,37 @@ define( 'CHILD_THEME_PPL_ASTRA_VERSION', '1.0.0' );
  */
 function child_enqueue_styles() {
 
-	wp_enqueue_style( 'ppl-astra-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_PPL_ASTRA_VERSION, 'all' );
+    wp_enqueue_style( 'ppl-astra-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_PPL_ASTRA_VERSION, 'all' );
 
 }
 
 add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
 
 function your_prefix_load_google_fonts() {
-	static $google_fonts = array();
-	if ( empty( $google_fonts ) ) {
-		$google_fonts_file = apply_filters( 'astra_google_fonts_json_file', ASTRA_THEME_DIR . 'assets/fonts/google-fonts.json' );
-		
-		if ( ! file_exists( ASTRA_THEME_DIR . 'assets/fonts/google-fonts.json' ) ) {
-			return array();
-		}
-		// Use file_get_contents instead of WP_Filesystem->get_contents()
-		$file_contants     = file_get_contents( $google_fonts_file );
-		$google_fonts_json = json_decode( $file_contants, 1 );
-		foreach ( $google_fonts_json as $key => $font ) {
-			$name = key( $font );
-			foreach ( $font[ $name ] as $font_key => $variant ) {
-				if ( stristr( $variant, 'italic' ) ) {
-					unset( $font[ $name ][ $font_key ] );
-				}
-				if ( 'regular' == $variant ) {
-					$font[ $name ][ $font_key ] = '400';
-				}
-				$google_fonts[ $name ] = array_values( $font[ $name ] );
-			}
-		}
-	}
-	return $google_fonts;
+    static $google_fonts = array();
+    if ( empty( $google_fonts ) ) {
+        $google_fonts_file = apply_filters( 'astra_google_fonts_json_file', ASTRA_THEME_DIR . 'assets/fonts/google-fonts.json' );
+        
+        if ( ! file_exists( ASTRA_THEME_DIR . 'assets/fonts/google-fonts.json' ) ) {
+            return array();
+        }
+        // Use file_get_contents instead of WP_Filesystem->get_contents()
+        $file_contants     = file_get_contents( $google_fonts_file );
+        $google_fonts_json = json_decode( $file_contants, 1 );
+        foreach ( $google_fonts_json as $key => $font ) {
+            $name = key( $font );
+            foreach ( $font[ $name ] as $font_key => $variant ) {
+                if ( stristr( $variant, 'italic' ) ) {
+                    unset( $font[ $name ][ $font_key ] );
+                }
+                if ( 'regular' == $variant ) {
+                    $font[ $name ][ $font_key ] = '400';
+                }
+                $google_fonts[ $name ] = array_values( $font[ $name ] );
+            }
+        }
+    }
+    return $google_fonts;
 }
 add_filter( 'astra_google_fonts', 'your_prefix_load_google_fonts' );
 
@@ -66,72 +66,68 @@ function donation_callback_wp() {
         $stripeToken  = $_POST['stripeToken'];
         $customerName = $_POST['custName'];
         $customerEmail = $_POST['custEmail'];
-    	
+        
         $customerAddress = $_POST['customerAddress'];
         $customerCity = $_POST['customerCity'];
         $customerZipcode = $_POST['customerZipcode'];
         $customerState = $_POST['customerState'];
         $customerCountry = $_POST['customerCountry'];
-    	
+        
         $cardNumber = $_POST['cardNumber'];
         $cardCVC = $_POST['cardCVC'];
         $cardExpMonth = $_POST['cardExpMonth'];
         $cardExpYear = $_POST['cardExpYear']; 
         
         // item details for which payment made
-    	$itemName = $_POST['item_details'];
-    	$itemNumber = $_POST['item_number'];
-    	$itemPrice = $_POST['price'];
-    	$totalAmount = $_POST['total_amount'];
-    	$currency = $_POST['currency_code'];
-    	$select_payment_type = $_POST['select_payment_type'];;
+        $itemName = $_POST['item_details'];
+        $itemNumber = $_POST['item_number'];
+        $itemPrice = $_POST['price'];
+        $totalAmount = $_POST['total_amount'];
+        $currency = $_POST['currency_code'];
+        $select_payment_type = $_POST['select_payment_type'];;
         
         //include Stripe PHP library
         require_once('stripe-php/init.php'); 
-        //set stripe secret key and publishable key
+        //set stripe secret key and publishable key  
         $stripe = array(
-          "secret_key"      => "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",
-          "publishable_key" => "pk_live_51NmNDEDapmKLlBZINog3R48dfcnnf9gfQs4whFiLU0hZyfPRbj6tC3hy2vKZE0HbiqnCqkSeRJCmt7DAZXj7QkG700d25iRh7h"
-        );   
-        /*$stripe = array(
-          "secret_key"      => "sk_test_51NmNDEDapmKLlBZId74hjCzq8q30ke7vXS4JsEzmYPOqLem46oZx4GQ7oS6BcU9GtlR2RpEqxN0KnuTaxxNdYjce00gAvetrRA",
-          "publishable_key" => "pk_test_51NmNDEDapmKLlBZI1eK4oBBIz7NJD35uLnl3zjAqTaINXs9UKVM4eahMEgmWCCgyYJ3hgapE7emQ9rShZyUSqd9Q00tFlAoyja"
-        );  */ 
+          "secret_key"      => "sk_test_xxxx",
+          "publishable_key" => "pk_test_yyyyy"
+        );
         \Stripe\Stripe::setApiKey($stripe['secret_key']);    
         
         //add customer to stripe
         try {
             $customer = \Stripe\Customer::create(array(
-        	'name' => $customerName,
-        	'description' => 'Donation',
+            'name' => $customerName,
+            'description' => 'Donation',
                 'email' => $customerEmail,
                 'source'  => $stripeToken,
-        	"address" => ["city" => $customerCity, "country" => $customerCountry, "line1" => $customerAddress, "line2" => "", "postal_code" => $customerZipcode, "state" => $customerState]
+            "address" => ["city" => $customerCity, "country" => $customerCountry, "line1" => $customerAddress, "line2" => "", "postal_code" => $customerZipcode, "state" => $customerState]
             ));  
         }catch (Exception $e) {
-    		  $body = $e->getJsonBody();
-    		  $err  = $body['error'];
-    		  echo $err_code  =  $err['code'];
-    		  $err_msg  = $err['message'];
-    		  exit();
-    	} 
-    	
-    	if($select_payment_type == "monthly"){
-    	    try { 
+              $body = $e->getJsonBody();
+              $err  = $body['error'];
+              echo $err_code  =  $err['code'];
+              $err_msg  = $err['message'];
+              exit();
+        } 
+        
+        if($select_payment_type == "monthly"){
+            try { 
                 // Create price with subscription info and interval 
                 $price = \Stripe\Price::create([ 
                     'unit_amount' => $totalAmount, 
                     'currency' => 'USD', 
                     'recurring' => ['interval' => 'month'],
-                    'product_data' => ['name' => 'Donation - irousa'], 
+                    'product_data' => ['name' => 'Donation - wxyz'], 
                 ]); 
             } catch (Exception $e) {  
                 $api_error = $e->getMessage(); 
                 $body = $e->getJsonBody();
-        		$err  = $body['error'];
-        		echo $err_code  =  $err['code'];
-        		$err_msg  = $err['message'];
-        		exit();
+                $err  = $body['error'];
+                echo $err_code  =  $err['code'];
+                $err_msg  = $err['message'];
+                exit();
             } 
              
             // details for which monthly payment performed
@@ -146,11 +142,11 @@ function donation_callback_wp() {
             }catch(Exception $e) { 
                   $api_error = $e->getMessage(); 
                   $body = $e->getJsonBody();
-        		  $err  = $body['error'];
-        		  $err_code  =  $err['code'];
-        		  $err_msg  = $err['message'];
+                  $err  = $body['error'];
+                  $err_code  =  $err['code'];
+                  $err_msg  = $err['message'];
                   echo "<script type=\"text/javascript\">alert('Error creating subscription:  $err_msg ($err_code)'); window.location.href='/donation-new/';</script>";
-        		  exit();
+                  exit();
             } 
             if(empty($api_error) && $subscription){ 
                 $_POST['subscriptionId'] = $subscription->id;
@@ -159,8 +155,8 @@ function donation_callback_wp() {
             }else{ 
                 //echo json_encode(['error' => $api_error]); 
             } 
-    	}else{
-    	    // details for which onetime payment performed
+        }else{
+            // details for which onetime payment performed
             try {
             $payDetails = \Stripe\Charge::create(array(
                 'customer' => $customer->id,
@@ -172,16 +168,16 @@ function donation_callback_wp() {
             if($payDetails->id!=""){
             send_stripe_email($customerName, $customerEmail, $totalAmount);}
             }catch (Exception $e) {
-        		  $body = $e->getJsonBody();
-        		  $err  = $body['error'];
-        		  $err_code  =  $err['code'];
-        		  $err_msg  = $err['message'];
-        		  echo "<script type=\"text/javascript\">alert('Card Error6:  $err_msg ($err_code)'); window.location.href='/donation-new/';</script>";
-        		  exit();
-        	} 
+                  $body = $e->getJsonBody();
+                  $err  = $body['error'];
+                  $err_code  =  $err['code'];
+                  $err_msg  = $err['message'];
+                  echo "<script type=\"text/javascript\">alert('Card Error6:  $err_msg ($err_code)'); window.location.href='/donation-new/';</script>";
+                  exit();
+            } 
             // get payment details
             $paymenyResponse = $payDetails->jsonSerialize();
-    	}
+        }
         wp_create_donation_user($_POST);
         $paymentMessage = "The payment was successful.";
     } else{
@@ -216,7 +212,7 @@ function send_stripe_email($customerName, $customerEmail, $totalAmount){
                                                 <table width="100%" cellpadding="0" cellspacing="0" border="0" style="border-bottom:1px solid #9f9f9f">
                                                   <tbody><tr>
                                                     <td align="center">
-                                                    <a href="https://www.irousa.org/"><img src="https://www.irousa.org/wp-content/uploads/2023/08/adam-alani-logo-final-1024x369.png" width="128" style="margin-bottom:30px;" alt="IRO"></a>
+                                                    <a href="https://www.wxyz.org/"><img src="https://www.wxyz.org/wp-content/uploads/2023/08/adam-alani-logo-final-1024x369.png" width="128" style="margin-bottom:30px;" alt="IRO"></a>
                                                     </td>
                                                   </tr>
                                                 </tbody></table>
@@ -229,7 +225,7 @@ function send_stripe_email($customerName, $customerEmail, $totalAmount){
                                                     <td colspan="12">
                                                       <h3 style="font-weight:normal;margin: 0 0 30px;font-size: 22px;color: #000;">'.$customerName.'! Your donations matters</h3>
                                                       <p style="font-size: 15px;line-height: 23px;color: #000;margin-bottom: 27px;">We have received your donation of $'.$totalAmount.'</p>
-                                                      <p style="font-size: 15px;line-height: 23px;color: #000;margin-bottom: 26px;">If you need futher assistance, please contact us at <a href="mailto:info@irousa.org" style="color: #018fd7;">info@irousa.org</a>.</p>
+                                                      <p style="font-size: 15px;line-height: 23px;color: #000;margin-bottom: 26px;">If you need futher assistance, please contact us at <a href="mailto:info@wxyz.org" style="color: #018fd7;">info@wxyz.org</a>.</p>
                                                     </td>
                                                   </tr>
                                                 </tbody></table>
@@ -240,7 +236,7 @@ function send_stripe_email($customerName, $customerEmail, $totalAmount){
                                                 <table width="100%" cellpadding="0" cellspacing="0" border="0" style="padding-bottom: 15px;padding-top: 0;">
                                                   <tbody><tr>
                                                     <td colspan="12">
-                                                      <p style="margin:0;font-size: 12px;line-height: 23px;color: #000;">© 2023 International Rights Organization Copyright | All Right Reserved<br/><a style="margin:0;font-size: 12px;line-height: 23px;color: #000;" href="https://www.irousa.org/" style="color: #018fd7;">www.irousa.org</a></p>
+                                                      <p style="margin:0;font-size: 12px;line-height: 23px;color: #000;">© 2023 International Rights Organization Copyright | All Right Reserved<br/><a style="margin:0;font-size: 12px;line-height: 23px;color: #000;" href="https://www.wxyz.org/" style="color: #018fd7;">www.wxyz.org</a></p>
                                                     </td>
                                                   </tr>
                                                 </tbody></table>
@@ -257,7 +253,7 @@ function send_stripe_email($customerName, $customerEmail, $totalAmount){
     
     
     $headers = array('Content-Type: text/html; charset=UTF-8');
-    $headers[] = "From: IRO <info@irousa.org>";
+    $headers[] = "From: IRO <info@wxyz.org>";
     wp_mail( $to, $subject, $message, $headers );
 }
 
@@ -268,7 +264,7 @@ function wp_create_donation_user($post) {
     
     $customerName = $_POST['custName'];
     $username = $customerEmail = $_POST['custEmail'];
-	
+    
     $customerAddress = $_POST['customerAddress'];
     $customerCity = $_POST['customerCity'];
     $customerZipcode = $_POST['customerZipcode'];
@@ -318,11 +314,11 @@ function wp_create_donation_user($post) {
 add_action('wp_ajax_donation_paypal', 'donation_paypal_callback_wp');
 add_action( 'wp_ajax_nopriv_donation_paypal', 'donation_paypal_callback_wp' );
 function donation_paypal_callback_wp() {
-	global $wpdb; // this is how you get access to the database
+    global $wpdb; // this is how you get access to the database
     $params = array();
     parse_str($_POST['myData'], $_POST);
-	wp_create_donation_user($_POST);
-	exit();
+    wp_create_donation_user($_POST);
+    exit();
 }
 
 //function added to display custom information about user in admin
